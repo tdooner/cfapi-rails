c06095471f3bd8d9488d909cbf43cb8c7eb80432
Tom Dooner <tomdooner@gmail.com>
2019-11-30 21:32:17 -0500

    Allow linking Salesforce and Github accounts

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,17 +1,27 @@
 module Users
   class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     def github
-      user = User.find_or_create_from_omniauth(request.env['omniauth.auth'])
+      if current_user
+        current_user.associate_oauth_identity(request.env['omniauth.auth'])
+        redirect_to edit_user_registration_path
+      else
+        user = User.find_or_create_from_omniauth(request.env['omniauth.auth'])
 
-      sign_in_and_redirect(user, event: :authentication)
-      set_flash_message(:notice, :success, kind: 'Github') if is_navigational_format?
+        sign_in_and_redirect(user, event: :authentication)
+        set_flash_message(:notice, :success, kind: 'Github') if is_navigational_format?
+      end
     end
 
     def salesforce
-      user = User.find_or_create_from_omniauth(request.env['omniauth.auth'])
+      if current_user
+        current_user.associate_oauth_identity(request.env['omniauth.auth'])
+        redirect_to edit_user_registration_path
+      else
+        user = User.find_or_create_from_omniauth(request.env['omniauth.auth'])
 
-      sign_in_and_redirect(user, event: :authentication)
-      set_flash_message(:notice, :success, kind: 'Salesforce') if is_navigational_format?
+        sign_in_and_redirect(user, event: :authentication)
+        set_flash_message(:notice, :success, kind: 'Salesforce') if is_navigational_format?
+      end
     end
 
     def meetup
