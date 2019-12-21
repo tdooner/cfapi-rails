d187b38c44bf3132f295613d81bef1778407b3b2
Tom Dooner <tomdooner@gmail.com>
2019-12-21 13:37:03 -0800

    Add a BrigadeProject spec for changed projects too

--- spec/models/brigade_project_spec.rb
+++ spec/models/brigade_project_spec.rb
@@ -5,26 +5,25 @@ RSpec.describe BrigadeProject do
     subject { described_class.replace_all_from_project_database(api_objects) }
 
     let(:brigade) { Brigade.create(name: 'Code for Tulsa') }
+    let(:valid_api_object) do
+      ApiObject::BrigadeProjectIndexEntry.new(
+        body: {
+          'organization' => {
+            'name' => brigade.name,
+          },
+          'projects' => [
+            {
+              'name' => 'Courtbot',
+              'link_url' => 'http://example.com/courtbot',
+              'code_url' => 'http://example.com/courtbot-code',
+            },
+          ],
+        }
+      )
+    end
 
     context 'with a new BrigadeProject' do
-      let(:api_objects) do
-        [
-          ApiObject::BrigadeProjectIndexEntry.new(
-            body: {
-              'organization' => {
-                'name' => brigade.name,
-              },
-              'projects' => [
-                {
-                  'name' => 'Courtbot',
-                  'link_url' => 'http://example.com/courtbot',
-                  'code_url' => 'http://example.com/courtbot-code',
-                },
-              ],
-            }
-          ),
-        ]
-      end
+      let(:api_objects) { [valid_api_object] }
 
       it 'creates a new BrigadeProject' do
         expect { subject }.to change(described_class, :count).by(1)
@@ -44,6 +43,28 @@ RSpec.describe BrigadeProject do
         expect(old_brigade_project).to be_persisted
         subject
         expect { old_brigade_project.reload }.to raise_error(ActiveRecord::RecordNotFound)
+      end
+    end
+
+    context 'with a changed project ApiObject' do
+      let(:original_api_object) { valid_api_object.dup }
+      let(:changed_api_object) do
+        valid_api_object.dup.tap do |api_object|
+          api_object.body['projects'][0]['code_url'] =
+            'http://example.com/some-other-code-url'
+        end
+      end
+      let(:api_objects) { [changed_api_object] }
+
+      before do
+        described_class.replace_all_from_project_database([original_api_object])
+      end
+
+      it 'updates the old BrigadeProject' do
+        expect { subject }
+          .to change { described_class.last.code_url }
+          .from('http://example.com/courtbot-code')
+          .to('http://example.com/some-other-code-url')
       end
     end
   end
