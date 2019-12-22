2ca31986f5399ebc0f545fd2d41ea0c544f3d973
Tom Dooner <tomdooner@gmail.com>
2019-12-22 16:13:56 -0500

    Add brigade project list to the Brigade's page

--- app/controllers/brigade_controller.rb
+++ app/controllers/brigade_controller.rb
@@ -2,5 +2,6 @@ class BrigadeController < ApplicationController
   def show
     @brigade = Brigade.find_by(name: params[:id])
     @meetup_page = @brigade.meetup
+    @projects = @brigade.brigade_projects
   end
 end
