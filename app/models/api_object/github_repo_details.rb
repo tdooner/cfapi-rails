7df9ea42f9e4f2be9d554eb0a062b14f8c1f0791
Tom Dooner <tomdooner@gmail.com>
2020-01-18 14:34:01 -0800

    Don't save DOCTYPE only records

--- app/models/api_object/github_repo_details.rb
+++ app/models/api_object/github_repo_details.rb
@@ -42,7 +42,10 @@ class ApiObject
       return unless body.present? && body['readme'].present?
 
       markdown = Base64.decode64(Hash[body['readme']]['content']).force_encoding('UTF-8')
-      Nokogiri::HTML(Redcarpet::Markdown.new(Redcarpet::Render::HTML).render(markdown)).to_xhtml
+      html = Redcarpet::Markdown.new(Redcarpet::Render::HTML).render(markdown)
+      return unless html.present?
+
+      Nokogiri::HTML(html).to_xhtml
     end
   end
 end
