19ac74ce4287a3199e080922b113d6a31f2070f2
Tom Dooner <tomdooner@gmail.com>
2020-02-13 16:50:10 -0800

    Add initial project search controller
    This won't work in production because the view isn't there.

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,42 @@
+class ProjectsController < ApplicationController
+  SearchResult = Struct.new(:brigade, :project, :search_properties) do
+  end
+
+  def index
+  end
+
+  def search
+    # turn the query into a tsquery
+    words = params[:q].scan(/"[\w ]+"|\w+/)
+    words[-1] = words[-1] + ":*"
+    q = words.join(' & ')
+
+    @results = BrigadeProject.connection.execute(BrigadeProject.sanitize_sql([<<-SQL.strip_heredoc, q, q])).map(&:to_h)
+      SELECT
+        brigade_projects_search.brigade_project_id,
+        ts_rank_cd(keywords, to_tsquery(?)) as rank,
+        brigade_projects.*
+      FROM brigade_projects_search
+      INNER JOIN brigade_projects
+        ON brigade_projects.id = brigade_project_id
+      INNER JOIN brigade_projects_search_fields
+        ON brigade_projects_search_fields.brigade_project_id = brigade_projects_search.brigade_project_id
+      WHERE ts_rank_cd(keywords, to_tsquery(?)) > 0
+      ORDER BY rank DESC
+      LIMIT 15;
+    SQL
+
+    ids = @results.map { |r| r['brigade_project_id'] }
+    @search_properties = BrigadeProject.connection.execute(BrigadeProject.sanitize_sql([<<-SQL.strip_heredoc, ids])).index_by { |r| r['brigade_project_id'] }
+      SELECT *
+      FROM brigade_projects_search_fields
+      WHERE brigade_project_id IN (?)
+    SQL
+
+    @brigades = Brigade.where(id: @results.map { |r| r['brigade_id'] }).index_by(&:id)
+
+    respond_to do |format|
+      format.json { render json: @results.map { |r| SearchResult.new(@brigades[r['brigade_id']], r, @search_properties[r['brigade_project_id']]) } }
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,9 @@ Rails.application.routes.draw do
     post :add_leader, on: :member
   end
 
+  resources :projects, only: %i[index] do
+    get 'search', on: :collection
+  end
+
   root to: 'home#show'
 end
