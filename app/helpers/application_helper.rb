35df16522afadeb6925b48d9eb045896202add2d
Tom Dooner <tomdooner@gmail.com>
2020-08-06 09:22:39 -0600

    Improve navbar
    * Comment out placeholder HTML
    * Add "active" status to track which page is current

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def nav_link_item(text, path)
+    content_tag('li', class: ['nav-item', ('active' if current_page?(path))]) do
+      link_to(path, class: 'nav-link') do
+        text
+      end
+    end
+  end
 end
